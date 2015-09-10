fdaea698d0f8d6f2aeac320f72fb6170724c79e7
Rahul Kumar <rahulkaitian@gmail.com>
2015-09-10 13:01:08 +0530

    Create DownloadFilefromSftp.java

---
+++ DownloadFilefromSftp.java
@@ -0,0 +1,10 @@
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Vector;
