e00fca202567d7e52ef51419dfec8538d7385fd5
Rahul Kumar <rahulkaitian@gmail.com>
2015-09-10 13:03:43 +0530

    Update DownloadFilefromSftp.java

--- DownloadFilefromSftp.java
+++ DownloadFilefromSftp.java
@@ -8,3 +8,30 @@ import java.io.IOException;
 
 import java.util.HashMap;
 import java.util.Vector;
+
+public class DownloadFilefromSftp {
+  
+      public static void main(String[] s) throws IOException, JSchException,
+                                               SftpException {
+        JSch jsch = new JSch();
+        Session session = null;
+        session =
+                jsch.getSession("username", "machineIp/hostname", 22);
+        session.setPassword("userpass");
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
+        ChannelSftp channel = null;
+        channel = (ChannelSftp)session.openChannel("sftp");
+        channel.connect();
+        String destinationPath = "C:\\logs\\";
+        channel.cd("/home/username/");
+        Vector<ChannelSftp.LsEntry> list = channel.ls("*.txt");
+        for (ChannelSftp.LsEntry entry : list) {
+            channel.get(entry.getFilename(),
+                            destinationPath + entry.getFilename());
+        }
+
+        channel.disconnect();
+        session.disconnect();
+    }
+}
